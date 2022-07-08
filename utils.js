5d5a3d4609dc5cc8f7bd1b1bfdac36a3c821f14f
James Salvatore <jimmy.c.salvatore@gmail.com>
2022-07-07 22:44:10 -0500

    chore: region select to modal;

---
+++ utils.js
@@ -0,0 +1,10 @@
+// function OpenModal() {
+//     $("#region-dialog").dialog({
+//     modal: true,
+//     buttons: {
+//         Ok: function () {
+//         $(this).dialog("close");
+//         },
+//     },
+//     });
+// }
